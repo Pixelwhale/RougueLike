32fe9bbc8a9655d4674c7501ca9c1c899800eff7
Pixelwhale <30408629+Pixelwhale@users.noreply.github.com>
2018-11-12 05:39:36 +0900

    [add] Item and ItemBackpack added

---
+++ RoguelikeProject/Assets/Original/Script/Item/Item.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item
+{
+    protected int ID;
+    protected int quantity;
+
+    public Item(int ID, int quantity)
+    {
+        this.ID = ID;
+        this.quantity = quantity;
+    }
+
+    public int GetID() { return ID; }
+    public int GetQuantity() { return quantity; }
+
+    public void AddQuantity(int q)
+    {
+        Debug.Assert(q > 0, "Item.AddQuantity(int q) : q <= 0");
+        quantity += q;
+    }
+
+    public abstract void Use(Completed.Player player);
+    protected void UseQuantity(int q)
+    {
+        Debug.Assert(q > 0, "Item.UseQuantity(int q) : q <= 0");
+        if (quantity < q)
+        {
+            Debug.Log("Quantity is not enough");
+            return;
+        }
+        quantity -= q;
+    }
+}
---
+++ RoguelikeProject/Assets/Original/Script/Item/ItemBackpack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBackpack
+{
+    public Completed.Player player;
+
+    private List<Item> items = new List<Item>(20);
+
+    public void Initialize()
+    {
+        items.Clear();
+    }
+
+    public void AddItem(Item item)
+    {
+        foreach (Item i in items)
+        {
+            if (i.GetID() == item.GetID())
+            {
+                i.AddQuantity(item.GetQuantity());
+                return;
+            }
+        }
+        AddNewItem(item);
+    }
+    private void AddNewItem(Item item)
+    {
+        if (items.Count == items.Capacity)
+        {
+            Debug.Log("Item list is full");
+            return;
+        }
+        items.Add(item);
+    }
+
+    public void UseItem(int index)
+    {
+        Debug.Assert(index >= 0 && index < items.Count, "UseItem : index invalid");
+        items[index].Use(player);
+    }
+}
